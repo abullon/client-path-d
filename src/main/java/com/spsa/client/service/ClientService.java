0d000e7a52191271ad948aa30181fe88afc6d291
Andres Matos <andres.matos@spsa.pe>
2019-07-12 09:00:08 -0500

    Enable CORS

--- src/main/java/com/spsa/client/service/ClientService.java
+++ src/main/java/com/spsa/client/service/ClientService.java
@@ -16,21 +16,25 @@ public class ClientService {
         this.clientController = clientController;
     }
 
+	@CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/creacliente")
     Client newClient(@RequestBody Client client) {
         return clientController.newClient(client);
     }
 
+	@CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/listclientes")
     public List<Client> getAllClients() {
         return clientController.getAllClients();
     }
 
+	@CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/deleteAllClients")
     public void deleteAllClients() {
         clientController.deleteAllClients();
     }
 
+	@CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/kpideclientes")
     public Kpi getKpiClientes() {
         return clientController.getKpiClientes();

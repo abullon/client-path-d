5a09ced40cddea2b263452c41e40ab03285e0e82
Andres Matos <andres.matos@spsa.pe>
2019-07-11 22:06:17 -0500

    Configuración para Heroku

--- src/main/java/com/spsa/client/model/Client.java
+++ src/main/java/com/spsa/client/model/Client.java
@@ -7,6 +7,7 @@ import com.spsa.client.util.DateUtils;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -15,12 +16,17 @@ public class Client {
 
     @FirebaseId
     private String id;
+    @NotNull(message = "El nombre es obligatorio")
     private String firstName;
+    @NotNull(message = "El apellido es obligatorio")
     private String lastName;
     private Integer age;
+    @NotNull(message = "La fecha de nacimiento es obligatoria")
     private Date birthday;
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "El genero es obligatorio")
     private Constant.Gender gender;
+    @NotNull(message = "El país es obligatorio")
     private String country;
     private Date probableDeathDate;
 

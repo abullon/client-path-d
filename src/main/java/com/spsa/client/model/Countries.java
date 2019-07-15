7ef44f8b84256288013b97251fdc74b4d27f25f6
Andres Matos <andres.matos@spsa.pe>
2019-07-15 11:17:04 -0500

    Agrega endpoint para obtener paises

---
+++ src/main/java/com/spsa/client/model/Countries.java
@@ -0,0 +1,16 @@
+package com.spsa.client.model;
+
+import java.util.List;
+
+public class Countries {
+
+    List<String> countries;
+
+    public List<String> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<String> countries) {
+        this.countries = countries;
+    }
+}
--- src/main/java/com/spsa/client/service/WorldPopulationService.java
+++ src/main/java/com/spsa/client/service/WorldPopulationService.java
@@ -1,21 +1,32 @@
 package com.spsa.client.service;
 
 import com.spsa.client.model.Client;
+import com.spsa.client.model.Countries;
 import com.spsa.client.model.LifeExpectancy;
 import com.spsa.client.util.DateUtils;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RestController
 public class WorldPopulationService {
 
     RestTemplate restTemplate = new RestTemplate();
 
     public Double getTotalLifeExpectancy(Client client) {
-        String url = "http://54.72.28.201:80/1.0/life-expectancy/total/"+client.getGender()+"/"+client.getCountry()+"/"+ DateUtils.format(client.getBirthday(),DateUtils.SMALL_DATE_FORMAT)+"/";
+        String url = "http://54.72.28.201:80/1.0/life-expectancy/total/" + client.getGender() + "/" + client.getCountry() + "/" + DateUtils.format(client.getBirthday(), DateUtils.SMALL_DATE_FORMAT) + "/";
         LifeExpectancy response = restTemplate.getForObject(
                 url,
                 LifeExpectancy.class);
-    return response.getTotalLifeExpectancy();
+        return response.getTotalLifeExpectancy();
+    }
+
+    @GetMapping("/countries")
+    public Countries getCountry() {
+        return  restTemplate.getForObject(
+                "http://54.72.28.201/1.0/countries",
+                Countries.class);
     }
 }
